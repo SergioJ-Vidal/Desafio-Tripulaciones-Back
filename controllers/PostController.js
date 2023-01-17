6c873fb423c2634586aa9d3901cf6c278e3b41a2
SergioJ-Vidal <113844469+SergioJ-Vidal@users.noreply.github.com>
2023-01-17 18:37:14 +0100

    Fixed merge and routes

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,6 +1,5 @@
 const { Post, User, Sequelize } = require("../models/index.js");
 const { Op } = Sequelize;
-const { post } = require("../routes/posts.js");
 
 const PostController = {
 
@@ -25,6 +24,66 @@ const PostController = {
           });
       });
   },
+
+  async getPostById(req, res) {
+    try {
+      const post = await Post.findByPk(req.params.id, {
+        include: [User],
+      });
+      res.send(post);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({
+        msg: "Ha habido un problema al traernos post por Id",
+        error,
+      });
+    }
+  },
+  async getPostByName(req, res) {
+    try {
+      const post = await Post.findOne({
+        where: {
+          title: {
+            [Op.like]: `%${req.params.title}%`,
+          },
+        },
+        include: [User],
+      });
+      res.send(post);
+    } catch (error) {}
+  },
+
+  async deletePost(req,res) {
+    try {
+      await Post.destroy({
+        where:{
+          id:req.params.id,
+        },
+      });
+      res.send({msg:"Publicacion eliminada con exito"})
+    } catch (error) {
+      console.error(err)
+      res
+        .status(500)
+        .send({msg:"Hubo un problema al eliminar el post", err})
+    }
+  },
+  async updatePostById(req,res){
+    try {
+      await Post.update({title:req.body.title, content:req.body.content},
+        {
+        where:{
+          id:req.params.id
+        },
+      })
+        res.send({msg:"Post actualizado con exito"})
+        }catch (error){
+            console.error(err)
+            res
+            .status(500)
+            .send({msg:"No se pudo actualizar el post", err})
+        }
+      },
 
 };
 module.exports = PostController;
