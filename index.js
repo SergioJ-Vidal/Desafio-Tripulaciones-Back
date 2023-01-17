960eebf699b8c821b8c856195104e242bb8c2b5d
SergioJ-Vidal <113844469+SergioJ-Vidal@users.noreply.github.com>
2023-01-17 17:48:57 +0100

    Update

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8000;
+
+require("dotenv").config();
 
 app.use(express.json())
 app.use("/users", require("./routes/users"))
 app.use("/posts", require("./routes/posts"))
+
 app.listen(PORT, ()=>console.log(`servidor levantado en el puerto ${PORT}`))
