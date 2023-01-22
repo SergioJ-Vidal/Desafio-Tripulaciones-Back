b55829343c08681d3e34e113f8cd20d6bb4a3e42
Miguel <mirkouda@gmail.com>
2023-01-22 18:47:57 +0100

    Develop changed

--- controllers/NewController.js
+++ controllers/NewController.js
@@ -3,13 +3,9 @@ const { Op } = Sequelize;
 
 const NewController = {
     async create(req, res) {
-
         if (req.file) req.body.image = req.file.filename
-
         try {
-
             const newN = await New.create({ ...req.body, UserId: req.user.id, CategoryId: req.body.category })
-
             res.status(201).send({ message: 'Noticia creada con éxito', newN });
         } catch (err) {
             err
@@ -20,25 +16,15 @@ const NewController = {
     },
 
     async getAll(req, res) {
-
         try {
-
             const news = await New.findAll({
-
                 include: Category,
-
             });
-
             res.send(news);
-
         } catch (error) {
-
             console.error(error);
-
         }
-
     },
-
 }
 
 module.exports = NewController;
