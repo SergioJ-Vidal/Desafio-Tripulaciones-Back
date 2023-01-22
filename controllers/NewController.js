a63f2267b45cdb7b4bae78dd10d12c552c6d0f1f
Miguel <mirkouda@gmail.com>
2023-01-22 18:43:03 +0100

    Develop merged

--- controllers/NewController.js
+++ controllers/NewController.js
@@ -3,42 +3,26 @@ const { Op } = Sequelize;
 
 const NewController = {
     async create(req, res) {
-
         if (req.file) req.body.image = req.file.filename
-
         try {
-
             const newN = await New.create({ ...req.body, UserId: req.user.id, CategoryId: req.body.category })
-
             res.status(201).send({ message: 'Noticia creada con éxito', newN });
         } catch (err) {
             err
             next(err)
-
         }
-
     },
 
     async getAll(req, res) {
-
         try {
-
             const news = await New.findAll({
-
                 include: Category,
-
             });
-
             res.send(news);
-
         } catch (error) {
-
             console.error(error);
-
         }
-
     },
-
 }
 
 module.exports = NewController;
