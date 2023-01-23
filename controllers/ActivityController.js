cfe115c228d0c3ec1eb3e383ac0c3255d331d767
SergioJ-Vidal <113844469+SergioJ-Vidal@users.noreply.github.com>
2023-01-23 18:30:41 +0100

    Debugging likes

--- controllers/ActivityController.js
+++ controllers/ActivityController.js
@@ -1,28 +1,37 @@
 const { Activity } = require("../models/index");
 
 const ActivityController = {
+
   async create(req, res) {
+
     try {
-      const ActivityY = await Activity.create({ ...req.body });
+      const ActivityY = await Activity.create({ ...req.body, UserId: req.user.id });
       res
         .status(201)
         .send({ message: "Actividad creada con éxito", ActivityY });
+
     } catch (error) {
+
       console.error(error);
       res
         .status(500)
         .send({ message: "Ha habido un problema al crear la actividad" });
     }
+
   },
 
   async deleteActivityById(req, res) {
+
     try {
+
       await Activity.destroy({
         where: {
           id: req.params.id,
         },
       });
+
       res.send({ msg: "Actividad eliminada con exito" });
+
     } catch (error) {
       console.error(err);
       res
@@ -32,29 +41,38 @@ const ActivityController = {
   },
 
   async getAllActivities(req, res) {
+
     try {
+
       const ActivityY = await Activity.findAll({});
       res.send(ActivityY);
     } catch (error) {
       console.error(error);
     }
+
   },
 
-  async updateActivityById (req,res){
+  async updateActivityById(req, res) {
+
     try {
-        await Activity.update({title:req.body.title, body:req.body.body, image:req.body.image, date:req.body.date },
-          {
-          where:{
-            id:req.params.id
+
+      await Activity.update({...req.body },
+        {
+          where: {
+            id: req.params.id
           },
         })
-          res.send({msg:"Actividad actualizada con exito"})
-          }catch (error){
-              console.error(err)
-              res
-              .status(500)
-              .send({msg:"No se pudo actualizar la actividad", err})
-          }
+
+      res.send({ msg: "Actividad actualizada con exito" })
+
+    } catch (error) {
+
+      console.error(err)
+      res
+        .status(500)
+        .send({ msg: "No se pudo actualizar la actividad", err })
+    }
+
   }
 };
 
