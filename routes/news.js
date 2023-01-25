5ee73c96deb767a634871584f37aecc8fa3e5eca
Miguel <mirkouda@gmail.com>
2023-01-26 00:03:05 +0100

    isAdmin deleted from some routes

--- routes/news.js
+++ routes/news.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const NewController = require('../controllers/NewController')
 const { uploadNewsImages } = require("../middleware/multer");
-const {authentication, isAdmin} = require('../middleware/authentication')
+const {authentication} = require('../middleware/authentication')
 
 
 router.post('/createNew', authentication, uploadNewsImages.single('image'), NewController.create)
--- routes/requests.js
+++ routes/requests.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { uploadRequestImages } = require("../middleware/multer");
 const RequestController = require('../controllers/RequestController')
-const { authentication, isAdmin } = require('../middleware/authentication')
+const { authentication } = require('../middleware/authentication')
 
 
 router.post('/createRequest', uploadRequestImages.single('image'), authentication, RequestController.createRequest)
